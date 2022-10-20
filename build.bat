63aa90a60613f502f3d3b2a88e93af1edced3486
Jackie Chen <cheezballz3.14@gmail.com>
2022-10-20 00:32:11 -0400

    More window shenanigans and update build script

--- build.bat
+++ build.bat
@@ -1,5 +1,9 @@
 @echo off
-call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+where /q cl
+if ERRORLEVEL 1 (
+    call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+)
 pushd build
-cl -Zi ..\src\main.cpp User32.lib
+cl -Zi ..\src\main.cpp User32.lib Gdi32.lib
 popd
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,53 @@
 #include <windows.h>
 
+LRESULT MainWindowCallback(
+  HWND Window,
+  UINT Message,
+  WPARAM wParam,
+  LPARAM lParam
+) {
+  LRESULT result = 0;
+
+  switch(Message) {
+    case WM_SIZE:
+    {
+      OutputDebugString("WM_SIZE\n");
+    } break;
+    case WM_DESTROY:
+    {
+      OutputDebugString("WM_DESTROY\n");
+    } break;
+    case WM_CLOSE:
+    {
+      OutputDebugString("WM_CLOSE\n");
+    } break;
+    case WM_ACTIVATEAPP:
+    {
+      OutputDebugString("WM_ACTIVATEAPP\n");
+    } break;
+    case WM_PAINT:
+    {
+      PAINTSTRUCT paint;
+      HDC deviceContext = BeginPaint(Window, &paint);
+      PatBlt(
+        deviceContext, 
+        paint.rcPaint.left, 
+        paint.rcPaint.top, 
+        paint.rcPaint.right - paint.rcPaint.left,
+        paint.rcPaint.bottom - paint.rcPaint.top,
+        BLACKNESS
+      );
+      EndPaint(Window, &paint);
+    } break;
+    default:
+    {
+      result = DefWindowProc(Window, Message, wParam, lParam);
+    } break;
+  }
+
+  return result;
+}
+
 int APIENTRY WinMain(
   HINSTANCE hInstance,
   HINSTANCE hPrevInstance,
@@ -7,6 +55,47 @@ int APIENTRY WinMain(
   int       nShowCmd
 ) {
     MessageBoxA(0, "Hello World!", "raika", MB_OK|MB_ICONINFORMATION);
+
+    WNDCLASSEX WindowClass = {};
+    WindowClass.cbSize = sizeof(WNDCLASSEX);
+    WindowClass.style = CS_OWNDC|CS_VREDRAW|CS_HREDRAW;
+    WindowClass.lpfnWndProc = MainWindowCallback;
+    WindowClass.hInstance = hInstance;
+    WindowClass.lpszClassName = "RaikaWindow";
+
+    if(RegisterClassEx(&WindowClass)) {
+      HWND WindowHandle = CreateWindowEx(
+        0,
+        WindowClass.lpszClassName,
+        "Raika",
+        WS_OVERLAPPEDWINDOW|WS_VISIBLE,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        0,
+        0,
+        hInstance,
+        0
+      );
+      if(WindowHandle) {
+        MSG message;
+        BOOL result;
+
+        while((result = GetMessage(&message,0,0,0)) != 0) {
+          if (result == -1) {
+            // Error
+          } else {
+            TranslateMessage(&message);
+            DispatchMessage(&message);
+          }
+        }
+      } else {
+        // Handle failed
+      }
+    } else {
+      // TODO: Window Failed to register
+    }
 
     return 0;
 }
