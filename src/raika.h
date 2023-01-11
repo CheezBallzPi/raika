ad20846e530b134d6530bc7301db99290aad405e
Jackie Chen <cheezballz3.14@gmail.com>
2023-01-10 21:45:50 -0500

    Pulled input out of platform

--- src/raika.h
+++ src/raika.h
@@ -1,4 +1,6 @@
 #if !defined(RAIKA_H)
+
+#define ArrayCount(array) (sizeof(array) / sizeof((array)[0]))
 // This includes everything that the platform will provide to the game
 // File IO
 
@@ -18,7 +20,24 @@ struct graphics_buffer {
   int pitch;
 };
 
-static void GameUpdateAndRender(graphics_buffer *graphicsBuffer, sound_buffer *soundBuffer);
+struct player_controller {
+  float lStickX;
+  float lStickY;
+  float rStickX;
+  float rStickY;
+  bool dpad[4];
+  bool buttons[20];
+};
+
+struct game_input {
+  struct player_controller controllers[4];
+};
+
+static void GameUpdateAndRender(
+  graphics_buffer *graphicsBuffer, 
+  sound_buffer *soundBuffer, 
+  game_input *gameInput
+);
 
 #define RAIKA_H
 #endif
