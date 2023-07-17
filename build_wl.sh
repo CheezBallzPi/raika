000696710978ee0cae681d18de29402429d36015
Jackie Chen <cheezballz3.14@gmail.com>
2023-07-16 22:00:00 -0400

    Add xdg headers to build

--- build_wl.sh
+++ build_wl.sh
@@ -2,4 +2,17 @@
 
 mkdir -p build
 cd build
-gcc $BUILD_OPTIONS -o raika -Wall ../src/wl_platform.cpp -lwayland-client -lm
+
+# XDG Shell headers
+if [ ! -f xdg-shell-protocol.c ]; then
+wayland-scanner private-code \
+  < /usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml \
+  > xdg-shell-protocol.c
+fi
+if [ ! -f xdg-shell-client-protocol.h ]; then
+wayland-scanner client-header \
+  < /usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml \
+  > xdg-shell-client-protocol.h
+fi
+
+gcc $BUILD_OPTIONS -o raika -Wall ../src/wl_platform.cpp xdg-shell-protocol.c -lwayland-client -lm -lrt
