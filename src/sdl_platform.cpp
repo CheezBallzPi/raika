5eb58842f173e7e38c289e399020659371d0598d
Jackie Chen <cheezballz3.14@gmail.com>
2023-10-03 20:54:44 -0400

    More vulkan stuff

--- src/sdl_platform.cpp
+++ src/sdl_platform.cpp
@@ -9,14 +9,17 @@
 
 #include <stdlib.h>
 #include <malloc.h>
+#include <string.h>
 
 // Globals
 static bool running = false;
 static SDL_Window *window = NULL;
 static SDL_Surface *windowSurface = NULL;
+static VkInstance vulkanInstance = NULL;
 
-const char *title = "Raika";
-
+static const char *title = "Raika";
+static const char* layers[1] = {"VK_LAYER_KHRONOS_validation"};
+static const int layerCount = 1;
 int initSDL() {
   // Init SDL
   if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) < 0) {
@@ -39,46 +42,31 @@ int initSDL() {
     SDL_Log("Failed to initialize window. Error: %s\n", SDL_GetError());
     return -1;
   }
-
-  windowSurface = SDL_GetWindowSurface(window);
-
-  if(windowSurface == NULL) {
-    SDL_Log("Failed to initialize surface. Error: %s\n", SDL_GetError());
-    return -1;
-  }
-  return 0;
 }
 
 int cleanupSDL() {
   // Cleanup
+  vkDestroyInstance(vulkanInstance, NULL);
   SDL_DestroyWindow(window);
   SDL_Quit();
   return 0;
 }
 
-SDL_Surface* loadImage(char* path) {
-  SDL_RWops* file = SDL_RWFromFile(path, "r");
-  if(file == NULL) {
-    SDL_Log("Failed to initialize file. Error: %s\n", SDL_GetError());
-    return NULL;
-  }
-  SDL_Surface* surface = SDL_LoadBMP_RW(file, 1);
-  if(surface == NULL) {
-    SDL_Log("Failed to initialize surface from file. Error: %s\n", SDL_GetError());
-    return NULL;
-  }
-  return surface;
-}
-
 int main(int argc, char *argv[]) {
   initSDL();
-  
+    
   uint32_t extensionCount = 0;
-  SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, NULL);
+  if(SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, NULL) != SDL_TRUE) {
+    SDL_Log("Error getting extensions: %s\n", SDL_GetError());
+    return -1;
+  };
   SDL_Log("Extension Count: %d\n", extensionCount);
   const char** extensions = (const char**) malloc(sizeof(const char*) * extensionCount);
   SDL_Vulkan_GetInstanceExtensions(window, &extensionCount, extensions);
   SDL_Log("Extensions written: %d\n", extensionCount);
+  for(int i = 0; i < extensionCount; i++) {
+    SDL_Log("Extension %d: %s\n", i + 1, extensions[i]);
+  }
 
   VkApplicationInfo appInfo = {};
   appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
@@ -88,27 +76,68 @@ int main(int argc, char *argv[]) {
   appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
   appInfo.apiVersion = VK_API_VERSION_1_3;
 
-  // SDL_UpdateWindowSurface(window);
+  uint32_t availableLayerCount;
+  vkEnumerateInstanceLayerProperties(&availableLayerCount, NULL);
+  SDL_Log("Layers count: %d\n", availableLayerCount);
+  VkLayerProperties* availableLayers = (VkLayerProperties*) malloc(sizeof(VkLayerProperties) * availableLayerCount);
+  vkEnumerateInstanceLayerProperties(&availableLayerCount, availableLayers);
+  SDL_Log("Layers written: %d\n", availableLayerCount);
+  for(int i = 0; i < availableLayerCount; i++) {
+    SDL_Log("Layer %d: %s\n", i + 1, availableLayers[i].layerName);
+  }
 
-  // SDL_Event event;
+  bool layerMissing = false;
 
-  // running = true;
-  // while(running) {
-  //   // Handle events
-  //   while(SDL_PollEvent(&event)) {
-  //     switch(event.type) {
-  //       case SDL_QUIT: {
-  //         printf("Quitting...\n");
-  //         running = false;
-  //         break;
-  //       }
-  //       default: {
-  //         printf("Unhandled Event: %d\n", event.type);
-  //         break;
-  //       }
-  //     }
-  //   }
-  // }
+  for(int i = 0; i < layerCount; i++) {
+    bool layerFound = false;
+    for(int j = 0; j < availableLayerCount; j++) {
+      if(strcmp(layers[i], availableLayers[j].layerName) == 0) {
+        layerFound = true;
+        break;
+      }
+    }
+    if(!layerFound) {
+      SDL_Log("Couldn't find layer %s\n", layers[i]);
+      layerMissing = true;
+    }
+  }
+
+  if(layerMissing) {
+    SDL_Log("Missing layer.\n");
+    return -1;
+  }
+
+  VkInstanceCreateInfo createInfo = {};
+  createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+  createInfo.pApplicationInfo = &appInfo;
+  createInfo.enabledExtensionCount = extensionCount;
+  createInfo.ppEnabledExtensionNames = extensions;
+  createInfo.enabledLayerCount = layerCount;
+  createInfo.ppEnabledLayerNames = layers;
+
+  if(vkCreateInstance(&createInfo, NULL, &vulkanInstance) != VK_SUCCESS) {
+    SDL_Log("Failed to make vulkan instance\n");
+  };
+
+  SDL_Event event;
+
+  running = true;
+  while(running) {
+    // Handle events
+    while(SDL_PollEvent(&event)) {
+      switch(event.type) {
+        case SDL_QUIT: {
+          printf("Quitting...\n");
+          running = false;
+          break;
+        }
+        default: {
+          printf("Unhandled Event: %d\n", event.type);
+          break;
+        }
+      }
+    }
+  }
 
   cleanupSDL();
   return 0;
