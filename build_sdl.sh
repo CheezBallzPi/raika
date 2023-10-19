89181f4de6daf5fd66c4bf31e591af9e11504a84
Jackie Chen <cheezballz3.14@gmail.com>
2023-10-18 21:23:47 -0400

    Add shaders

--- build_sdl.sh
+++ build_sdl.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+set -e
+
 mkdir -p build
 cd build
+
+glslc ../shaders/shader.vert -o vert.spv
+glslc ../shaders/shader.frag -o frag.spv
 
 g++ $BUILD_OPTIONS -o raika -Wall ../src/sdl_platform.cpp \
   -lm -lvulkan `sdl2-config --cflags --libs`
